f281d3c330d782f273e205e37cf6346663263cf7
sh710 <sh710@DESKTOP-GIKO6MC>
2023-03-20 19:17:41 +0100

    Tablero hecho

--- src/main/java/ieseuropa/Tablero.java
+++ src/main/java/ieseuropa/Tablero.java
@@ -1,5 +1,46 @@
 package main.java.ieseuropa;
 
+import java.util.ArrayList;
+
 public class Tablero {
+	
+	private ArrayList<Columna> tablero;
+	private ArrayList<Carta> mazo;
+	private ArrayList<Jugador> jugadores;
+	
+	public Tablero() {
+		tablero = new ArrayList<>();
+		mazo = new ArrayList<>();
+		jugadores = new ArrayList<>();
+	}
+
+	public ArrayList<Columna> getTablero() {
+		return tablero;
+	}
+
+	public void setTablero(ArrayList<Columna> tablero) {
+		this.tablero = tablero;
+	}
+
+	public ArrayList<Carta> getMazo() {
+		return mazo;
+	}
+
+	public void setMazo(ArrayList<Carta> mazo) {
+		this.mazo = mazo;
+	}
+
+	public ArrayList<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	@Override
+	public String toString() {
+		return "Tablero [tablero=" + tablero + ", mazo=" + mazo + ", jugadores=" + jugadores + "]";
+	}
 
 }
